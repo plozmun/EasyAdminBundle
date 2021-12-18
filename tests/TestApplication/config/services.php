84d256a8f8407b8478b47544fa278c314ae309b2
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-12-18 17:38:06 +0100

    -

--- tests/TestApplication/config/services.php
+++ tests/TestApplication/config/services.php
@@ -2,6 +2,9 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\Controller\CategoryCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\Controller\DashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\Controller\SecureDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\DataFixtures\AppFixtures;
 
 return static function (ContainerConfigurator $container) {
@@ -16,8 +19,17 @@ return static function (ContainerConfigurator $container) {
     $services->load('EasyCorp\\Bundle\\EasyAdminBundle\\Tests\\TestApplication\\', '../src/*')
         ->exclude('../{Entity,Tests,Kernel.php}');
 
-    $services->load('EasyCorp\\Bundle\\EasyAdminBundle\\Tests\\TestApplication\\Controller\\', '../src/Controller/')
-        ->tag('controller.service_arguments');
+    $services->set(DashboardController::class)
+        ->tag('controller.service_arguments')
+        ->call('setContainer', [service('service_container')]);
+
+    $services->set(SecureDashboardController::class)
+        ->tag('controller.service_arguments')
+        ->call('setContainer', [service('service_container')]);
+
+    $services->set(CategoryCrudController::class)
+        ->tag('controller.service_arguments')
+        ->call('setContainer', [service('service_container')]);
 
     $services->set(AppFixtures::class)->tag('doctrine.fixture.orm');
 };
